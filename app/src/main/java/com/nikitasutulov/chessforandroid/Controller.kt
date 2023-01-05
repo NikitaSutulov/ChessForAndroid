6a041881eda89690b8526fb80f00fec2c4304b11
Nikita Sutulov <notsid212@gmail.com>
2023-01-05 11:01:37 +0200

    minor code style change

--- app/src/main/java/com/nikitasutulov/chessforandroid/Controller.kt
+++ app/src/main/java/com/nikitasutulov/chessforandroid/Controller.kt
@@ -8,12 +8,13 @@ class Controller () {
         val model = Model(activity)
 
         val startResetButton: Button = activity.requireViewById(R.id.start_reset_button)
+        val pauseResumeButton: Button = activity.requireViewById(R.id.pause_resume_button)
+
         startResetButton.setOnClickListener {
             model.startOrResetGame()
             model.swapButtonText(startResetButton, "Start", "Reset")
         }
 
-        val pauseResumeButton: Button = activity.requireViewById(R.id.pause_resume_button)
         pauseResumeButton.setOnClickListener {
             model.pauseOrResumeGame()
             model.swapButtonText(pauseResumeButton, "Pause", "Resume")
